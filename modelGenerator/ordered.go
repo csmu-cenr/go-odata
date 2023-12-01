d6ed7030a81e35f53ac04ba7d9a3c3216582c84d
Keith John Hutchison <keith@bd2l.com.au>
2023-12-01 20:33:51 +1030

    Added func sortedCaseInsensitiveStringKeys.
    Changes to be committed:
    	modified:   modelGenerator/ordered.go

--- modelGenerator/ordered.go
+++ modelGenerator/ordered.go
@@ -1,18 +1,43 @@
 package modelGenerator
 
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 type ordered interface {
 	int | int64 | string
 }
 
 func sortedKeys[V any, K ordered](m map[K]V) []K {
+
 	keys := make([]K, len(m))
 	i := 0
 	for k := range m {
 		keys[i] = k
 		i++
 	}
+
 	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+
+	return keys
+}
+
+func sortedCaseInsensitiveStringKeys[V any](m map[string]V) []string {
+
+	keys := make([]string, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+
+	// A case insensitive sort
+	sort.Slice(keys, func(i, j int) bool {
+		left := strings.ToLower(keys[i])
+		right := strings.ToLower(keys[j])
+		return left < right
+	})
+
 	return keys
 }
