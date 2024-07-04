770635cec085fc67a8b7d9d7c9fdceccabac08a7
Keith John Hutchison <keith@bd2l.com.au>
2024-07-04 10:54:04 +0930

    Added more examples for ExtraFields. Added constants.
    Changes to be committed:
    	modified:   extras/ExtraFields.go
    	new file:   extras/consts.go

--- extras/ExtraFields.go
+++ extras/ExtraFields.go
@@ -1,13 +1,15 @@
 package dataModel
 
+// ExtraFields used when extracting nullable.Nullable[T].Selected data
 func ExtraFields() []string {
 	/*
-		ODataId       string `json:"@odata.id,omitempty"`
-		ODataEditLink string `json:"@odata.editLink,omitempty"`
-		ODataEtag     string `json:"@odata.etag,omitempty"`
-		ODataReadLink string `json:"@odata.readLink,omitempty"`
-		RowId         int    `json:"ROWID,omitempty"`
-		RowModId      int    `json:"ROWMODID,omitempty"`
+		ODataId             string `json:"@odata.id,omitempty"`
+		ODataEditLink       string `json:"@odata.editLink,omitempty"`
+		ODataNavigationLink string `json:"@odata.navigationLink,omitempty"`
+		ODataEtag           string `json:"@odata.etag,omitempty"`
+		ODataReadLink       string `json:"@odata.readLink,omitempty"`
+		RowId               int    `json:"ROWID,omitempty"`
+		RowModId            int    `json:"ROWMODID,omitempty"`
 	*/
 	//result := []string{`ODataId`, `ODataEditLink`, `ODataEtag`, `ODataReadLink`, `RowId`, `RowModId`}
 	result := []string{`ODataId`, `ODataEditLink`, `RowId`, `RowModId`}
---
+++ extras/consts.go
@@ -0,0 +1,25 @@
+package dataModel
+
+// OData constants used in FileMaker Pro
+// Used here to make it easy to swap the dollar signs in or out.
+// When they are no longer required by FileMaker Pro.
+const (
+	COUNT               = `$count`
+	CROSSJOIN           = `$crossjoin`
+	EXPAND              = `$expand`
+	FILTER              = `$filter`
+	FORMAT              = `$format`
+	NEXTLINK            = `$nextLink`
+	METADATA            = `$metadata`
+	ODATAEDITLINK       = `$odataeditlink`       // not required or supported by FileMaker. Added to facilitate extraction of @odata.editLink
+	ODATAID             = `$odataid`             // not required or supported by FileMaker. Added to facilitate extraction of @odata.id
+	ODATANAVIGATIONLINK = `$odatanavigationlink` // not supported yet. Will facilitate extraction of @odata.navigationLink
+	ODATAREADLINK       = `$odatareadlink`       // not supported yet. Will facilitate extraction of @odata.readLink
+	ORDERBY             = `$orderby`
+	REF                 = `$ref`
+	SELECT              = `$select`
+	SKIP                = `$skip`
+	TOP                 = `$top`
+	TRUE                = `true`
+	VALUE               = `$value`
+)
