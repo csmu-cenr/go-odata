5165b27139d9ce4f37d6286a6fe736bcce2aa24f
Keith John Hutchison <keith@bd2l.com.au>
2024-11-30 14:29:40 +1030

    Some minor edits.
    Changes to be committed:
    	modified:   extras/DefaultURLValues.go

--- extras/DefaultURLValues.go
+++ extras/DefaultURLValues.go
@@ -9,7 +9,7 @@ func DefaultURLValues(rowModId int) url.Values {
 
 	values := url.Values{}
 	if rowModId > 0 {
-		//
+		// ROWMODID is the version control field in the odata source if it has one.
 		filter := fmt.Sprintf(`%s eq %d`, ROWMODID, rowModId)
 		values.Set(FILTER, filter)
 	}
