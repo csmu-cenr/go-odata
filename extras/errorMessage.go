94c1df773818861282c8adbdaaaf1fe21aaa6ffe
Keith John Hutchison <keith@bd2l.com.au>
2026-01-11 08:36:54 +1030

    Updated error message to standard in internal project
    	modified:   extras/errorMessage.go

--- extras/errorMessage.go
+++ extras/errorMessage.go
@@ -5,19 +5,26 @@ import (
 )
 
 type ErrorMessage struct {
-	Attempted  string      `json:"attemped,omitempty"`
-	Code       string      `json:"code"`
-	Details    interface{} `json:"details"`
-	ErrorNo    int         `json:"errorNo,omitempty"`
-	Function   string      `json:"function,omitempty"`
-	InnerError interface{} `json:"err,omitempty"`
-	IPAddress  string      `json:"ipaddress,omitempty"`
-	Message    string      `json:"message,omitempty"`
-	Payload    interface{} `json:"payload,omitempty"`
-	RequestUrl string      `jsono:"requestUrl,omitempty"`
-	Stack      []string    `json:"stack,omitempty"`
-	Timestamp  string      `json:"timestamp,omitempty"`
-	Timezone   string      `json:"timezone,omitempty"`
+	Attempted     string   `json:"attemped,omitempty"`
+	Code          string   `json:"code"`
+	Details       any      `json:"details"`
+	ErrorNo       int      `json:"errorNo,omitempty"`
+	Exit          string   `json:"exit"`
+	FileName      string   `json:"filename,omitempty"`
+	Function      string   `json:"function,omitempty"`
+	InnerError    any      `json:"err,omitempty"`
+	IPAddress     string   `json:"ipaddress,omitempty"`
+	LineNumber    int      `json:"lineNumber,omitempty"`
+	Link          string   `json:"link,omitempty"`
+	Message       string   `json:"message,omitempty"`
+	Path          string   `json:"path,omitempty"`
+	Payload       any      `json:"payload,omitempty"`
+	RequestUrl    string   `json:"requestUrl,omitempty"`
+	Stack         []string `json:"stack,omitempty"`
+	Timestamp     string   `json:"timestamp,omitempty"`
+	Timezone      string   `json:"timezone,omitempty"`
+	UnixTimestamp int64    `json:"unixTimestamp"`
+	User          any      `json:"user,omitempty"`
 }
 
 func (e ErrorMessage) Error() string {
