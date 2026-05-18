2385667015dd5c78cff5161b04758a070b85e86d
Keith John Hutchison <keith@bd2l.com.au>
2026-05-18 11:15:01 +0930

    Removed toKeys

--- modelGenerator/edmx.go
+++ modelGenerator/edmx.go
@@ -134,18 +134,18 @@ type rawEdmxDataServices struct {
 	Schemas []rawEdmxSchema `xml:"Schema"`
 }
 
-func (ds *rawEdmxDataServices) toKeys() map[int][]string {
-	var keys map[int][]string
-	keys = make(map[int][]string)
-	index := 0
-	for _, schema := range ds.Schemas {
-		for _, entityType := range schema.EntityTypes {
-			keys[index] = append(keys[index], entityType.Name)
-		}
-		index++
-	}
-	return keys
-}
+// func (ds *rawEdmxDataServices) toKeys() map[int][]string {
+// 	var keys map[int][]string
+// 	keys = make(map[int][]string)
+// 	index := 0
+// 	for _, schema := range ds.Schemas {
+// 		for _, entityType := range schema.EntityTypes {
+// 			keys[index] = append(keys[index], entityType.Name)
+// 		}
+// 		index++
+// 	}
+// 	return keys
+// }
 
 func (ds *rawEdmxDataServices) toDataService() edmxDataServices {
 	dataService := &edmxDataServices{Schemas: map[string]edmxSchema{}}
